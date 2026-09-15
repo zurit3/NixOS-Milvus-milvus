652f43c6c572150cd9e25a4fe96afad6c8744020
zuri3 <z-l-f@hotmail.com>
2026-09-15 15:25:13 +0100

    point ssh to my key file so auto recognise works after upgrades and reboots

--- System-Config/configuration.nix
+++ System-Config/configuration.nix
@@ -117,7 +117,13 @@
       publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
     };
   };
-  programs.ssh.startAgent = true;
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      AddKeysToAgent yes
+      IdentityFile ~/.ssh/id_ed25519
+    '';
+  };
 
   #boot stuff
   boot.loader.efi.canTouchEfiVariables = true;
